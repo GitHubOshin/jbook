7dea4741de73772d3329699b0d228b17ec13327f
OSHIN <izkp44@gmail.com>
2023-09-25 13:30:44 +0700

    Update code-cell.tsx

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,31 +1,35 @@
-import 'bulmaswatch/superhero/bulmaswatch.min.css'
-import { useState, useEffect } from 'react'
+import './code-cell.css'
+import { useEffect } from 'react'
 import CodeEditor from './code-editor'
 import Preview from './Preview'
-import bundle from '../bundler'
 import Resizable from './Resizable'
 import ResizableVertical from './ResizableVertical'
 import { Cell } from '../state/cell'
 import { useActions } from '../hooks/use-actions'
+import { useTypedSelector } from '../hooks/use-typed-selector'
 
 interface CellCellProps {
   cell: Cell
 }
 const CodeCell: React.FC<CellCellProps> = ({ cell }) => {
-  const [error, setError] = useState('')
-  const [code, setCode] = useState('')
-  const { updateCell } = useActions()
+  const { updateCell, createBundle } = useActions()
+  const bundle = useTypedSelector((state) => state.bundles[cell.id])
 
   useEffect(() => {
+    if (!bundle) {
+      createBundle(cell.id, cell.content)
+      return
+    }
+
     const timer = setTimeout(async () => {
-      const output = await bundle(cell.content)
-      setCode(output.err)
+      createBundle(cell.id, cell.content)
     }, 750)
 
     return () => {
       clearTimeout(timer)
     }
-  }, [cell.content])
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cell.content, cell.id, createBundle])
 
   return (
     <Resizable>
@@ -43,7 +47,17 @@ const CodeCell: React.FC<CellCellProps> = ({ cell }) => {
             onChange={(eventValue) => updateCell(cell.id, eventValue)}
           />
         </ResizableVertical>
-        <Preview code={code} error={error} />
+        <div className="progress-wrapper">
+          {!bundle || bundle.loading ? (
+            <div className="progress-cover">
+              <progress className="progress is-small is-primary" max="100">
+                Loading...
+              </progress>
+            </div>
+          ) : (
+            <Preview code={bundle.code} error={bundle.error} />
+          )}
+        </div>
       </div>
     </Resizable>
   )
